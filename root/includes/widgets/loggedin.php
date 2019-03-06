37d78c1ff34c2b60c2d7f9bcdb1d1caa52d8ba25
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 23:29:35 +0530

    Update loggedin.php

--- root/includes/widgets/loggedin.php
+++ root/includes/widgets/loggedin.php
@@ -2,6 +2,5 @@
     <h2>Hello, <?php echo $user_data['first_name'] ?> </h2>
     <div class="inner">
         links <br>
-        <a href="logout.php"><button>logout</button></a>
     </div>
 </div>
