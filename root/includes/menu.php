66ef6d863df01181ac367c0bd8d44a2f3013d080
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 17:18:30 +0530

    Create menu.php

---
+++ root/includes/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="forum.php">Forum</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+    </ul>
+</nav>
