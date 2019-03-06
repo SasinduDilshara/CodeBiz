7ad518d37a97f4b56c6637a32b637c4aac81daa2
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 17:19:01 +0530

    Create login.php

---
+++ root/login.php
@@ -0,0 +1,35 @@
+<?php 
+include 'core/init.php';
+
+if (empty($_POST) === false) {
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	if (empty($email) === true || empty($password) === true) {
+		$errors[] = 'You need to enter an email and password';	
+	} else if (user_exists($email) === false) {
+		$errors[] = 'We can\'t find your email. Have you registered?';
+	} else if (user_active($email) === false) {
+		$errors[] = 'You haven\'t activated your account';
+	} else {
+		$login = login($email, $password);
+		if ($login === false) {
+			$errors[] = 'Email/password is incorrect';
+		} else {
+			//set the user session 
+			$_SESSION['user_id'] = $login;
+			//redirect to homepage
+			header('Location: index.php');
+			exit();
+		}
+	}
+
+	//print_r($errors);
+} else {
+	echo 'landed on php';
+}
+include 'includes/overall/header.php';
+
+echo output_errors($errors);
+include 'includes/overall/footer.php';
+?>
