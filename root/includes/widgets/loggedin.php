109325c3ab7897f99f29b45856bc96007f970196
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 17:18:45 +0530

    Create loggedin.php

---
+++ root/includes/widgets/loggedin.php
@@ -0,0 +1,7 @@
+<div class="widget">
+    <h2>Hello, <?php echo $user_data['first_name'] ?> </h2>
+    <div class="inner">
+        links <br>
+        <a href="logout.php"><button>logout</button></a>
+    </div>
+</div>
