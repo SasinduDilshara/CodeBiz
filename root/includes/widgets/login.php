e03019e65723e2ba2e4c6da94959c7d026f5f1f3
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 17:18:51 +0530

    Create login.php

---
+++ root/includes/widgets/login.php
@@ -0,0 +1,23 @@
+<div class="widget">
+    <h2>Log in/Register</h2>
+    <div class="inner">
+        <form action="login.php" method="post">
+        	<ul id="login">
+        		<li>
+        			Email:<br>
+        			<input type="text" name="email">
+        		</li>
+        		<li>
+        			Password:
+        			<input type="password" name="password">
+        		</li>
+        		<li>
+        			<input type="submit" value="Log in">
+        		</li>
+        		<li>
+        			<a href="register.php">Register</a>
+        		</li>
+        	</ul>
+        </form>
+    </div>
+</div>
