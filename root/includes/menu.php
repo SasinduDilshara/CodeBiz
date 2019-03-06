1b9be065115e5b2d1b15554617341ea357b038cb
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 23:29:28 +0530

    Update menu.php

--- root/includes/menu.php
+++ root/includes/menu.php
@@ -1,8 +1,20 @@
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="forum.php">Forum</a></li>
-        <li><a href="contact.php">Contact us</a></li>
+        <?php
+		if (logged_in() === true) {
+			if ($user_data['type'] === 'customer') {
+				echo '<li><a href="postrequset.php">Add request</a></li>';
+			}
+			else if ($user_data['type'] === 'provider') {
+				echo '<li><a href="postad.php">Post An Ad</a></li>';
+			}
+			echo '<li><a href="edituser.php">Edit profile</a></li>';
+			echo '<li><a href="logout.php">Log Out</a></li>';
+		} else {
+		 	echo '<li><a href="register.php">Register</a></li>';
+		}
+		?>
+
     </ul>
 </nav>
