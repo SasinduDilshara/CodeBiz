343724defae676dd79c6bd1ccd25c6a0b11fdb37
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-06 17:19:18 +0530

    Create site.php

---
+++ root/site.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+	//$myVar = "WTF";
+	echo("<p>Hello world</p>");
+	//echo("I am going to print $myVar here");
+	 ?>
+</body>
+</html>
