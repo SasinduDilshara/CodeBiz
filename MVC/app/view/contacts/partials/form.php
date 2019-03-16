85c7043c3a276f933539e038752d7929b73c611c
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-16 18:56:43 +0530

    Create form.php

---
+++ MVC/app/view/contacts/partials/form.php
@@ -0,0 +1,24 @@
+<form class="form" action=<?=$this->postAction?> method="post">
+
+	<div><?=$this->displayErrors ?></div>
+	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
+	<?= input_block('text','First name','fname',$this->contact->fname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Last name','lname',$this->contact->lname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Address','address',$this->contact->address,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Email','email',$this->contact->email,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Service','service',$this->contact->service,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+	<?= input_block('text','Home Phone','home_phone',$this->contact->home_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
+	<?= input_block('text','Age','age',$this->contact->age,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+	<?= input_block('text','Mobile','cell_phone',$this->contact->cell_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
+	<?= input_block('text','Work_phone','work_phone',$this->contact->work_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
+	<?= input_block('text','State','state',$this->contact->state,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+	<?= input_block('text','Service Type','serviceType',$this->contact->serviceType,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
+
+	<div class = "col-md-12 text-right">
+		<a href="<?=PROOT?>contacts" class="btn btn-default"> Cancel </a>
+
+	<?= submitBlock('Save',['class'=>'btn btn-primary']) ?>
+
+	</div>
+
+</form>
