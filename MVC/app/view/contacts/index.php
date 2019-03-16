aa0869131cc94402300e56060387ba00663eb244
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-17 01:15:33 +0530

    Update index.php

--- MVC/app/view/contacts/index.php
+++ MVC/app/view/contacts/index.php
@@ -1,5 +1,5 @@
 
-
+<?php $this->setSiteTitle("Contacts"); ?>
 <?php $this->start('body'); ?>
 <h1 class ="text-center red"> My Contacts </h1>
 <table class="table table-striped table-condensed table-bordered">
@@ -25,7 +25,9 @@
 				<td><?= $contact->email; ?></td>
 				<td><?= $contact->cell_phone; ?></td>
 				<td><?= $contact->home_phone; ?></td>
-				<td><a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
+				<td><a href="<?=PROOT?>contacts/edit/<?=$contact->id?>" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> Edit </a>
+
+				<a href="<?=PROOT?>contacts/delete/<?=$contact->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure to Delete <?=$contact->displayName()?>')){return false;}"><i class="glyphicon glyphicon-remove"></i> Delete </a></td>
 			</tr>
 
 		<?php endforeach; ?>
