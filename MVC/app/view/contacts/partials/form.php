e7e9ecee54dbe5b22c3adf74afcb534add5ad120
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-16 23:59:35 +0530

    Update form.php

--- MVC/app/view/contacts/partials/form.php
+++ MVC/app/view/contacts/partials/form.php
@@ -5,6 +5,7 @@
 	<?= input_block('text','First name','fname',$this->contact->fname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 	<?= input_block('text','Last name','lname',$this->contact->lname,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 	<?= input_block('text','Address','address',$this->contact->address,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','City','city',$this->contact->city,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 	<?= input_block('text','Email','email',$this->contact->email,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 	<?= input_block('text','Service','service',$this->contact->service,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
 	<?= input_block('text','Home Phone','home_phone',$this->contact->home_phone,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
