49ae9e6df2ed02554122ff0a11009a4b6174c6e5
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-16 23:59:15 +0530

    Update Contacts.php

--- MVC/app/model/Contacts.php
+++ MVC/app/model/Contacts.php
@@ -2,7 +2,7 @@
 
 class Contacts extends Model
 {
-	public function __construct($user='')
+	public function __construct($contact='')
 	{
 		$table = 'contacts';
 		parent:: __construct($table);
@@ -13,7 +13,7 @@ class Contacts extends Model
 
 	public function findByUserId($userId,$params=[])
 	{
-		$conditions = ['condition'=>'user_id = ?','bind'=>[$userId]
+		$conditions = ['conditions'=>'user_id = ?','bind'=>[$userId]
 	];
 	$conditions = array_merge($conditions,$params);
 	return $this->find($conditions);
@@ -39,6 +39,54 @@ class Contacts extends Model
 			// 'max' => 155
 		] 
 	];
+
+
+	public function findByIdAndUserId($contact_id,$user_id,$params=[])
+	{
+		// dnd($contact_id);
+		// dnd($user_id);
+		$conditions =
+		 [
+		'conditions' => 'id = ? AND user_id = ?',
+		'bind' => [$contact_id , $user_id]
+	];
+
+	// dnd($conditions);
+	$conditions = array_merge($conditions,$params);
+	// dnd($conditions);
+
+	return $this->findFirst($conditions);
+	}
+
+	public function displayAddress()
+	{
+		$address = '';
+		if(!empty($this->address))
+		{
+			$address.=$this->address."<br>";
+
+		}
+		// if(!empty($this->address1))
+		// {
+		// 	$address.=$this->address1."<br>"; //if two or more address
+
+		// }
+		if(!empty($this->city))
+		{
+			$address.=$this->city.",";
+		}
+
+			$address.=$this->state." ".$this->zip."<br>";
+		// }
+		return $address;
+	}
+
+	public function displayAddressLabel()
+	{
+		$html = $this->displayName()."<br>";
+		$html .= $this->displayAddress();
+		return $html;
+	}
 
 }
 
