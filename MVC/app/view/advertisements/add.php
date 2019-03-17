a458ba91e26e3589c55c208f446c1c7879f48e1a
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-17 15:07:01 +0530

    Create add.php

---
+++ MVC/app/view/advertisements/add.php
@@ -0,0 +1,9 @@
+<?php $this->setSiteTitle("Add Advertisements"); ?>
+<?php $this->start('body'); ?>
+
+<div class="col-md-8 col-md-offset-2 well">
+	<h2 class="text-center"> Add a Advertisement </h2>
+	<?php $this->partial('advertisements','form'); ?>
+</div>
+
+<?php $this->end(); ?>
