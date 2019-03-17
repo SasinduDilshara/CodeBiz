5e32f1507a570e003be0b8a9e17e730b56606e75
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-17 15:07:06 +0530

    Create Advertisements.php

---
+++ MVC/app/model/Advertisements.php
@@ -0,0 +1,93 @@
+<?php
+
+class Advertisements extends Model
+{
+	public function __construct($advertisement='')
+	{
+		$table = 'advertisements';
+		parent:: __construct($table);
+		$this->_softDelete = true;
+	}
+
+	public $deleted =0;
+
+	public function findByUserId($userId,$params=[])
+	{
+		$conditions = ['conditions'=>'user_id = ?','bind'=>[$userId]
+	];
+	$conditions = array_merge($conditions,$params);
+	return $this->find($conditions);
+	}
+
+	public function displayName()
+	{
+		return $this->name.' '.$this->topic;
+	}
+
+	public static $addValidation =
+	[
+		'name' =>[
+			'display' =>'Name',
+			'required' => true,
+			'min' => 6
+	],
+	
+		'topic' =>
+		[
+			'display' =>'Topic',
+			'required' => true
+			// 'max' => 155
+		] 
+	];
+
+
+	public function findByIdAndUserId($advertisement_id,$user_id,$params=[])
+	{
+		// dnd($contact_id);
+		// dnd($user_id);
+		$conditions =
+		 [
+		'conditions' => 'id = ? AND user_id = ?',
+		'bind' => [$advertisement_id , $user_id]
+	];
+
+	// dnd($conditions);
+	$conditions = array_merge($conditions,$params);
+	// dnd($conditions);
+
+	return $this->findFirst($conditions);
+	}
+
+	public function displayAdd()
+	{
+		$address = '';
+		if(!empty($this->name))
+		{
+			$address.=$this->name."<br>";
+
+		}
+		// if(!empty($this->address1))
+		// {
+		// 	$address.=$this->address1."<br>"; //if two or more address
+
+		// }
+		if(!empty($this->topic))
+		{
+			$address.=$this->topic.",";
+		}
+
+			// $address.=$this->state." ".$this->zip."<br>";
+		// }
+		return $address;
+	}
+
+	public function displayAddLabel()
+	{
+		$html = $this->displayName()."<br>";
+		$html .= $this->displayAdd();
+		return $html;
+	}
+
+}
+
+?>
