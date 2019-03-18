be3e36614b23a209fc8897629ff2f206476d2e52
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-18 13:01:54 +0530

    Update add.php

--- MVC/app/view/advertisements/add.php
+++ MVC/app/view/advertisements/add.php
@@ -3,7 +3,7 @@
 
 <div class="col-md-8 col-md-offset-2 well">
 	<h2 class="text-center"> Add a Advertisement </h2>
-	<?php $this->partial('advertisements','form'); ?>
+	<?php $this->partial('advertisements','form') ?>
 </div>
 
 <?php $this->end(); ?>
