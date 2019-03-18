6510b61312e88b8218d4c2ca948ea1060c7c4dae
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-19 00:36:00 +0530

    Update login.php

--- MVC/app/view/register/login.php
+++ MVC/app/view/register/login.php
@@ -4,42 +4,31 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<div class = "col-md-6 col-offset-3 well">
-<form class="form" action="<?=PROOT?>register/login" method="post">
-	<div<?= $this->displayErrors; ?> </div>
-	<h3 class="text-center">Sign In</h3>
-<div class="form-group">
-	<label for="username" > Username </label>
-	  <input type="text" name="username" id="username" class = "form-control">
-</div>
-
-<div class="form-group">
-	<label for="password" > Password </label>
-	  <input type="text" name="password" id="password" class="form-control" size ="10">
-</div>
-
-<div class="form-group">
-	<label for="remember_me" > Remember_me 
-	  <input type="checkbox" name="remember_me" id="remember_me" value="on">
-	</label>
-</div>
-
-<div class="form-group">
-	  <input type="submit" class ="btn btn-large btn-primary" value="Login">
-</div>
-
-<div >
-	  <a href="<?=PROOT?>register/register" class ="text-primary"> Register </a>
-</div>
-<br>
-
-<p> Forget password? <p>
+<div class = "container">
+	<h3 class="text-center p-4">Sign In</h3>
+	<form class="form" action="<?=PROOT?>register/login" method="post" >
+		<div class="form-group">
+			<input type="text" name="username" id="username" class = "form-control" placeholder = "Username">
+		</div>
+		<div class="form-group">
+			<input type="text" name="password" id="password" class="form-control" placeholder = "Password">
+		</div>
+		<div class="form-check"> 
+			<input type="checkbox" class="form-check-input" name="remember_me" id="remember_me" value="on">
+			<label class="form-check-label" for="remember_me" > Remember me
+			</label>
+		</div>
+		<div> <?= $this->displayErrors; ?> </div>
+		<div class="form-group">
+			<input type="submit" class ="btn btn-primary" value="Login">
+		</div>
+	</form>
 	
+	<div>
+		<a href="<?=PROOT?>register/register" class ="text-primary"> Register </a> <br>
+		<a href = #  class ="text-primary"> Forget password ? </a>
+	</div>
 
-<div >
-	   <a href = #  class ="text-primary"> Forget password </a>
 </div>
-
-
 
 <?php $this->end(); ?>
