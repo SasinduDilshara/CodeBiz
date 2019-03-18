d0f4affb0219882a8e22aceb2ec7afdcd3bcf7eb
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-19 01:49:23 +0530

    Update Users.php

--- MVC/app/model/Users.php
+++ MVC/app/model/Users.php
@@ -222,12 +222,6 @@ class Users extends Model
                     // 'required' => true
                     //'max' => 100
                 ],
-                'age' => [
-                    'display' => 'Age',
-                    // 'required' => true,
-                    'min' => 2
-                    //'max' => 100
-                ],
 
                 'confirm' => [
                     'display' => 'Confirm Password',
