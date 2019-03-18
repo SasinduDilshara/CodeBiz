adaa2d9e1f0aa768d570092a16c0ea91ea675b2d
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-03-19 02:59:08 +0530

    Update index.php

--- MVC/app/view/accounts/index.php
+++ MVC/app/view/accounts/index.php
@@ -3,18 +3,15 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<div class="col-md-8 col-md-offset-2 well">
+<div class="container">
 	
-	<h2 class="text-center"><?=$this->account->username?></h2>
-	<div class="col-md-6">
+	<h2 class="text-center text-info"><?=$this->account->username?></h2>
+	<div class="col-md-6 bg-light p-3 rounded">
 		<p><strong><pre> Name :  </strong><?=$this->account->displayName()?></pre></p>
 		<p><strong><pre> Email :  </strong><?=$this->account->email?></pre></p>
 		<p><strong><pre> Contact :  </strong><?=$this->account->phoneNumber2?></pre></p>
 		<p><strong><pre> Address :  </strong><?=$this->account->displayAddress()?></pre></p>
 		<p><strong><pre> Type of User :  </strong><?=$this->account->displayType()?></pre></p>
-	</div>
-	<div class="col-md-6">
-		<?=$this->account->displayAddressLabel()?>
 	</div>
 
 	<a href="<?=PROOT?>accounts/edit/<?=$this->account->id?>" class="btn btn-xs btn-default"> Edit </a>
