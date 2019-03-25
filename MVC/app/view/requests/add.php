321e70db2b1a12ba07a41ea9ce796a2f6840f8ef
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-25 17:41:28 +0530

    Create add.php

---
+++ MVC/app/view/requests/add.php
@@ -0,0 +1,9 @@
+<?php $this->setSiteTitle("Add Requests"); ?>
+<?php $this->start('body'); ?>
+
+<div class="col-md-8 col-md-offset-2 well">
+	<h2 class="text-center"> Add a Contact </h2>
+	<?php $this->partial('requests','form'); ?>
+</div>
+
+<?php $this->end(); ?>
