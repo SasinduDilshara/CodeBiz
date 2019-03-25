6c52ad1ca74b95dc14c377c28c10854e168f5a1a
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-25 17:41:13 +0530

    Create Requests.php

---
+++ MVC/app/model/Requests.php
@@ -0,0 +1,93 @@
+<?php
+
+class Requests extends Model
+{
+	public function __construct($request='')
+	{
+		$table = 'requests';
+		parent:: __construct($table);
+		$this->_softDelete = true;
+	}
+
+	public $deleted =0;
+	
+
+	// public $customer =currentUser()->username;
+
+	public function findByUserId($userId,$params=[])
+	{
+		$conditions = ['conditions'=>'user_id = ?','bind'=>[$userId]
+	];
+	$conditions = array_merge($conditions,$params);
+	return $this->find($conditions);
+	}
+
+	public function displayName()
+	{
+		return $this->service;;
+	}
+
+	public static $addValidation =
+	[
+		'service' =>[
+			'display' =>'Service',
+			'required' => true,
+			'min' => 6
+	],
+	
+		'description' =>
+		[
+			'display' =>'Description',
+			'required' => true
+			// 'max' => 155
+		] 
+	];
+
+
+	public function findByIdAndUserId($request_id,$user_id,$params=[])
+	{
+		// dnd($request_id,);
+		// dnd($user_id);
+		$conditions = ['conditions' => 'id = ? AND user_id = ?','bind' => [$request_id,$user_id]
+	];
+
+	// dnd($conditions);
+	$conditions = array_merge($conditions,$params);
+	// dnd($conditions);
+
+	return $this->findFirst($conditions);
+	}
+
+	// public function displayAddress()
+	// {
+	// 	$address = '';
+	// 	if(!empty($this->address))
+	// 	{
+	// 		$address.=$this->address."<br>";
+
+	// 	}
+	// 	// if(!empty($this->address1))
+	// 	// {
+	// 	// 	$address.=$this->address1."<br>"; //if two or more address
+
+	// 	// }
+	// 	if(!empty($this->city))
+	// 	{
+	// 		$address.=$this->city.",";
+	// 	}
+
+	// 		$address.=$this->state." ".$this->zip."<br>";
+	// 	// }
+	// 	return $address;
+	// }
+
+	// public function displayAddressLabel()
+	// {
+	// 	$html = $this->displayName()."<br>";
+	// 	$html .= $this->displayAddress();
+	// 	return $html;
+	// }
+
+}
+
+?>
