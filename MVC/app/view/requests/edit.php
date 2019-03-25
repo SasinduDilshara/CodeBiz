3350bd53c351f11f8ca257ffe48ec6a97c6e4265
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-25 17:41:38 +0530

    Create edit.php

---
+++ MVC/app/view/requests/edit.php
@@ -0,0 +1,21 @@
+<?php $this->setSiteTitle('Edit Requests'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<div class="col-md-8 col-md-offset-2 well">
+<!-- 	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
+ -->	
+ <h2 class="text-center"><?=$this->request->displayName()?></h2>
+ <?= $this->partial('requests','form')?>
+	<!-- <div class="col-md-6">
+		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
+		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
+		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
+	</div>
+	<div class="col-md-6">
+		<?=$this->contact->displayAddressLabel()?>
+	</div>
+</div> -->
+
+<?php $this->end(); ?>
