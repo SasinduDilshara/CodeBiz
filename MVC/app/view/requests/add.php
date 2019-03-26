92578369797762489e9ab5ae73238b74334d94d8
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-26 10:34:34 +0530

    Update add.php

--- MVC/app/view/requests/add.php
+++ MVC/app/view/requests/add.php
@@ -2,7 +2,7 @@
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<h2 class="text-center"> Add a Contact </h2>
+	<h2 class="text-center"> Add a Request </h2>
 	<?php $this->partial('requests','form'); ?>
 </div>
 
