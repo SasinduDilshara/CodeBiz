fa6d33e7ab8d4d791edfcd40df378b7c045bd0ae
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-26 10:34:30 +0530

    Update details.php

--- MVC/app/view/contacts/details.php
+++ MVC/app/view/contacts/details.php
@@ -5,13 +5,12 @@
 
 <div class="container">
 	<a href="<?=PROOT?>contacts" class="btn btn-xs btn-default"> Back</a>
-<<<<<<< HEAD
+<!-- <<<<<<< HEAD -->
 	<h2 class="text-center"><?=$this->contact->displayName();?></h2>
 	<div class="col-md-6">
-=======
-	<h2 class="text-center"><?=$this->contact->displayaddName()?></h2>
+<!-- ======= -->
 	<div class="col-md-6 bg-light p-3 rounded">
->>>>>>> 1f1bb71e756f597a12666060c25a04c1b31bb98d
+<!-- >>>>>>> 1f1bb71e756f597a12666060c25a04c1b31bb98d -->
 		<p><strong><pre> First :  </strong><?=$this->contact->age?></pre></p>
 		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
 		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
