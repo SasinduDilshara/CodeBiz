5124f462f4b3e32a3a3d16d8256494124272e16e
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-26 08:51:22 +0530

    Create search.php

---
+++ MVC/app/view/advertisements/search.php
@@ -0,0 +1,30 @@
+<?php $this->setSiteTitle("Search results"); ?>
+<?php $this->start('body'); ?>
+<h1 class ="text-center red"> Search Results </h1>
+<table class="table table-striped table-condensed table-bordered">
+	<thead>
+		<th> Name</th>
+		<th> Brand</th>
+		<th> Description</th>
+		<th></th>
+		<th></th>
+	</thead>
+	<body>
+		<?php foreach($this->advertisements as $advertisement): ?>
+
+			<tr>
+
+				<td>
+					<a 
+					href="<?=PROOT?>advertisements/details/<?=$advertisement->id?>">
+					<?= $advertisement->name; ?>
+				</a>
+			</td>
+				<td><?= $advertisement->brand; ?></td>
+				<td><?= $advertisement->description; ?></td>	
+			</tr>
+
+		<?php endforeach; ?>
+	</body>
+</table>
+<?php $this->end(); ?>
