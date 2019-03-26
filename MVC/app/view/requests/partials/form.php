a6d67aaa55e9c2d53fe1753413250b3b979fda9d
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-26 10:34:44 +0530

    Update form.php

--- MVC/app/view/requests/partials/form.php
+++ MVC/app/view/requests/partials/form.php
@@ -2,8 +2,8 @@
 
 	<div><?=$this->displayErrors ?></div>
 	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-	<?= input_block('text','First name','service',$this->request->service,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Last name','description',$this->request->description,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Service','service',$this->request->service,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Description','description',$this->request->description,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 	<?= input_block('text','Address','provider',$this->request->provider,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
 
 
