e95c59a098a7bd381f55c449b8a940052956de92
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-26 08:55:16 +0530

    Update details.php

--- MVC/app/view/advertisements/details.php
+++ MVC/app/view/advertisements/details.php
@@ -1,4 +1,4 @@
-
+@ -1,22 +0,0 @@
 <?php $this->setSiteTitle($this->advertisement->displayName()); ?>
 <?php $this->start('head'); ?>
 <?php $this->end(); ?>
@@ -8,7 +8,7 @@
 	<a href="<?=PROOT?>advertisements" class="btn btn-xs btn-default"> Back</a>
 	<h2 class="text-center"><?=$this->advertisement->displayName()?></h2>
 	<div class="col-md-6">
-
+		<p><strong><pre> Name :  </strong><?=$this->advertisement->name?></pre></p>
 		<p><strong><pre> Topic :  </strong><?=$this->advertisement->topic?></pre></p>
 		<p><strong><pre> Brand :  </strong><?=$this->advertisement->brand?></pre></p>
 		<p><strong><pre> Description :  </strong><?=$this->advertisement->description?></pre></p>
