eb0db9478cd127fc6964b4f3c107185e9441fe03
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-03-30 16:59:09 +0530

    Update ToolsController.php

--- MVC/app/controller/ToolsController.php
+++ MVC/app/controller/ToolsController.php
@@ -8,22 +8,22 @@ class ToolsController extends Controller
 		$this->view->setLayout('defaultlay');
 	}
 
-	public function indexAction()
-	{
-		$this->view->render('tools/index');
-	}
-	public function firstAction()
-	{
-		$this->view->render('tools/first');
-	}
-	public function secondAction()
-	{
-		$this->view->render('tools/second');
-	}
-	public function thirdAction()
-	{
-		$this->view->render('tools/third');
-	}
+	// public function indexAction()
+	// {
+	// 	$this->view->render('tools/index');
+	// }
+	// public function firstAction()
+	// {
+	// 	$this->view->render('tools/first');
+	// }
+	// public function secondAction()
+	// {
+	// 	$this->view->render('tools/second');
+	// }
+	// public function thirdAction()
+	// {
+	// 	$this->view->render('tools/third');
+	// }
 }
 
 ?>
