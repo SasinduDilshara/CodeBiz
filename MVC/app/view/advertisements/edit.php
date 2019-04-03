c20ec5001e4000c54f9627fa92735fa12fffe822
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:01:15 +0530

    Update edit.php

--- MVC/app/view/advertisements/edit.php
+++ MVC/app/view/advertisements/edit.php
@@ -4,17 +4,8 @@
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">	
- <h2 class="text-center"><?=$this->advertisement->displayName()?></h2>
+ <h2 class="text-center"><?=$this->advertisement->topic?></h2>
  <?= $this->partial('advertisements','form')?>
-	<!-- <div class="col-md-6">
-		<p><strong><pre> Age :  </strong><?=$this->contact->age?></pre></p>
-		<p><strong><pre> Email :  </strong><?=$this->contact->email?></pre></p>
-		<p><strong><pre> Mobile :  </strong><?=$this->contact->cell_phone?></pre></p>
-		<p><strong><pre> Home :  </strong><?=$this->contact->home_phone?></pre></p>
-	</div>
-	<div class="col-md-6">
 
-	</div>
-</div> -->
 
 <?php $this->end(); ?>
