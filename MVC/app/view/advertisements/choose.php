ec83688fe9a60c75488cc59611e8e1e891511f39
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:01:23 +0530

    Create choose.php

---
+++ MVC/app/view/advertisements/choose.php
@@ -0,0 +1,16 @@
+<?php $this->setSiteTitle('Edit Advertisement'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+	<h2 class="text-center"> Choose the addvertisement type</h2>
+
+				<a href="<?=PROOT?>advertisements/add/Cleaning" class="btn btn-danger btn-xs"><i class="glyphicon glyphicon-remove"></i> Cleaning Advertisement </a></td>
+
+				<a href="<?=PROOT?>advertisements/add/Catering" class="btn btn-danger btn-xs"><i class="glyphicon glyphicon-remove"></i> Catering Advertisement </a></td>
+
+
+				<a href="<?=PROOT?>advertisements/add/Laundering" class="btn btn-danger btn-xs"><i class="glyphicon glyphicon-remove"></i> Laundering Advertisement </a></td>
+
+
+<?php $this->end(); ?>
