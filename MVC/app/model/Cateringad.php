0e0b870cdbefae41062af3c4652c8e4de75db9e8
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:01:45 +0530

    Create Cateringad.php

---
+++ MVC/app/model/Cateringad.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cateringad extends Advertisements
+{
+	public function __construct($cateringad='')
+	{
+		// $table = 'cateringad';
+		parent:: __construct('cateringad');
+		$this->_softDelete = false;
+		$this->type = 'Catering';
+	}
+
+	public function displayAdd()
+	{
+		if(!empty($this->area))
+		{
+			return $this->area."<br>".$this->capacity;
+		}
+
+	}
+
+	public function displayAddLabel()
+	{
+		$html = $this->displayName()."<br>";
+		// $html .= $this->displayAdd();
+		return $html;
+	}
+
+}
+
+?>
