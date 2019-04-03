6f3e2e89aab8c1ff534d644ec9433c54197d9598
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:00:53 +0530

    Update details.php

--- MVC/app/view/requests/details.php
+++ MVC/app/view/requests/details.php
@@ -1,16 +1,14 @@
-<?php $this->setSiteTitle($this->request->displayName()); ?>
+<?php $this->setSiteTitle($this->request->service); ?>
 <?php $this->start('head'); ?>
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
 <div class="col-md-8 col-md-offset-2 well">
-	<a href="<?=PROOT?>request" class="btn btn-xs btn-default"> Back</a>
-	<h2 class="text-center"><?=$this->request->displayName()?></h2>
+	<a href="<?=PROOT?>requests" class="btn btn-xs btn-default"> Back</a>
+	<h2 class="text-center"><?=$this->request->service?></h2>
 	<div class="col-md-6">
-		<p><strong><pre> Details :  </strong><?=$this->request->service?></pre></p>
-		<p><strong><pre> Customer :  </strong><?=$this->request->customer?></pre></p>
 		<p><strong><pre> Description :  </strong><?=$this->request->description?></pre></p>
-		<p><strong><pre> Provider :  </strong><?=$this->request->provider?></pre></p>
+		
 	</div>
 </div>
 
