6dbc56ea7f9c33bc74d32226faf333feddadbd82
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:01:33 +0530

    Create Launderingad.php

---
+++ MVC/app/model/Launderingad.php
@@ -0,0 +1,33 @@
+<?php
+
+class Launderingad extends Advertisements
+{
+	public function __construct($launderingad='')
+	{
+		// $table = 'cleaningad';
+		parent:: __construct('launderingad');
+		$this->_softDelete = false;
+		$this->type = 'Laundering';
+	}
+
+	public function displayAdd()
+	{
+		// $address = '';
+		if(!empty($this->area))
+		{
+			return $this->area."<br>".$this->capacity;
+		}
+		// // }
+		
+	}
+
+	public function displayAddLabel()
+	{
+		$html = $this->displayName()."<br>";
+		// $html .= $this->displayAdd();
+		return $html;
+	}
+
+}
+
+?>
