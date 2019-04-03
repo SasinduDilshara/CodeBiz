1e771ebd10fe0767e87915776a8b55a3e17e8f31
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-03 23:01:06 +0530

    Update form.php

--- MVC/app/view/advertisements/partials/form.php
+++ MVC/app/view/advertisements/partials/form.php
@@ -2,30 +2,12 @@
 
 	<div><?=$this->displayErrors ?></div>
 	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-	<?= input_block('text','Location','location',$this->advertisement->location,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<div>
-	    <label for="topic"> Topic</label><br>
-	<!-- <pre> -->
-	      <input type="radio" name="topic" id="topic"  value="Laundry" checked> Laundry<br>
-	      <input type="radio" name="topic" id="topic"  value="Cleaning"> Cleaning<br>
-	      <input type="radio" name="topic" id="topic"  value="Meals"> Meals <br>
-	  <!-- </pre> -->
-	</div>
+	<?= input_block('text','Topic','topic',$this->advertisement->topic,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+	<?= input_block('text','Location','area',$this->advertisement->area,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
+
 	<br>
 	<?= input_block('text','Description','description',$this->advertisement->description,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-	<?= input_block('text','Contact','contactnumber',$this->advertisement->contactnumber,['class'=>'form-control'],['class'=>'form-group col-md-4']) ?>
-	<?= input_block('text','Links','links',$this->advertisement->links,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<div>
-	    <label for="customerType"> Delivery service Available</label><br>
-	<!-- <pre> -->
-	      <input type="radio" name="customerType"name="customerType" id="customerType"   value="delivery" checked> Yes<br>
-	      <input type="radio" name="customerType"name="customerType" id="customerType"   value="NoDelivery"> NO <br>
-	  <!-- </pre> -->
-	</div>
 	
-	<?= input_block('text','Brand','brand',$this->advertisement->brand,['class'=>'form-control'],['class'=>'form-group col-md-3']) ?>
-
-
 	<div class = "col-md-12 text-right">
 		<a href="<?=PROOT?>advertisements" class="btn btn-default"> Cancel </a>
 
