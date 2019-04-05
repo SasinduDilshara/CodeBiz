66854f8d4ca2ea9e1175078e3ab69a46295deee7
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-05 12:41:52 +0530

    Update messagesbutton.php

--- MVC/app/view/home/messagesbutton.php
+++ MVC/app/view/home/messagesbutton.php
@@ -1,4 +1,4 @@
-<?php if(currentUser()->userType == 'Customer' || currentUser()->userType == 'Provider'): ?>
+<?php if(currentUser()): ?>
 <div class = "col-md-12 text-center">
 <a href="<?=PROOT?>messages/show" class="btn btn-default"> Show Messages </a>
 </div>
