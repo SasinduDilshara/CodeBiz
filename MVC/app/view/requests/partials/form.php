7ae33f3dc71231518e8cc40f51c100d307bec540
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-04-06 19:49:11 +0530

    Edit request form

--- MVC/app/view/requests/partials/form.php
+++ MVC/app/view/requests/partials/form.php
@@ -1,18 +1,20 @@
 <form class="form" action=<?=$this->postAction?> method="post">
-
+	<div class="form-group col">
+		<?= input_block('text','Service','service',$this->request->service,['class'=>'form-control'],['class'=>'form-group']) ?>
+	</div>
+	<div class="form-group col">
+		<?= input_block('text','Description','description',$this->request->description,['class'=>'form-control'],['class'=>'form-group']) ?>
+	</div>
+	<div class="form-group col">
+		<?= input_block('text','Location','area',currentUser()->area,['class'=>'form-control'],['class'=>'form-group']) ?>
+		<ul class="list-group" id="result"></ul>
+	</div>
 	<div><?=$this->displayErrors ?></div>
-	<!-- HIIIIIIIIIIIIIIIIIIIIIIIIIII -->
-	<?= input_block('text','Service','service',$this->request->service,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Description','description',$this->request->description,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-	<?= input_block('text','Location','area',currentUser()->area,['class'=>'form-control'],['class'=>'form-group col-md-6']) ?>
-
+	<div class="form-group" style="text-align: center;">
 		<?= submitBlock('Save',['class'=>'btn btn-primary']) ?>
-		
-	<div class = "col-md-12 text-right">
+	</div>
+	<div class = "form-group text-right">
 		<a href="<?=PROOT?>requests" class="btn btn-default"> Cancel </a>
-
-
-
 	</div>
 
 </form>
