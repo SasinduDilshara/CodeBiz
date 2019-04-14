7c6686c6ad9770805b6e5f376c05bc69ffd439d1
Shammi <shammi0107@gmail.com>
2019-04-14 23:23:42 +0530

    search

--- MVC/app/view/advertisements/search.php
+++ MVC/app/view/advertisements/search.php
@@ -7,12 +7,16 @@
 		<th> Description</th>
 <!-- 		<th> Description</th> -->
 		<th> Location</th>
-		<th></th>
+		<th>Type</th>
+		<th>Provider</th>
 		<th></th>
 	</thead>
 	<body>
 		<?php foreach($this->advertisements as $advertisement): ?>
-
+			<?php if(currentUser()): ?>
+			<?php $customer = currentUser()->findById((int)($advertisement->user_id)) ?>
+			<?php endif; ?>
+			<?php if(!currentUser() || $customer->username != currentUser()->username): ?>
 			<tr>
 
 				<td>
@@ -23,8 +27,41 @@
 			</td>
 
 				<td><?= $advertisement->description; ?></td>	
-				<td><?= $advertisement->area; ?></td>	
+				<td><?= $advertisement->area; ?></td>
+				<td> <?= $advertisement->type;?> </td>
+				<td><a   
+			href="<?=PROOT?>accounts/details/<?=$advertisement->user_id?>">
+			<?php if(!currentUser()): ?>
+			Service Providerdetails
+		<?php endif; ?>
+			<?php if(currentUser()): ?>
+					<?= $customer->username; ?>
+			<?php endif; ?>
+			</a>
+		</td>	
+			
+			<td>
+
+				<?php if(!currentUser() ):?>
+				<a href="<?=PROOT?>advertisements/accept/<?=$advertisement->id?>/<?=$advertisement->user_id?>/<?=$advertisement->type?>" class="btn btn-danger btn-xs"></i> Want Service </a>
+				
+
+				 <?php elseif(!in_array((string)(currentUser()->id),explode(",",$advertisement->CustomerId)) ):?>
+				<a href="<?=PROOT?>advertisements/accept/<?=$advertisement->id?>/<?=$advertisement->user_id?>/<?=$advertisement->type?>" class="btn btn-danger btn-xs"></i> Want Service </a>
+				
+				<?php elseif(in_array((string)(currentUser()->id),explode(",",$advertisement->CustomerId))) :?>
+				<a href="<?=PROOT?>advertisements/cancel/<?=$advertisement->id?>/<?=$advertisement->user_id?>/<?=$advertisement->type?>" class="btn btn-danger btn-xs"></i> Cancel Service </a>
+				<?php endif; ?>
+			</td>
+			<td>
+			<?php if(currentUser() && in_array((string)(currentUser()->id),explode(",",$advertisement->confirmCustomerId))):?>
+			<!-- <td> -->
+					<a href="<?=PROOT?>advertisements/showChat/<?=$advertisement->id?>/<?= $advertisement->type?>" class="btn btn-danger btn-xs"></i> Show Chat </a>
+				<!-- </td> -->
+			<?php endif; ?>
+		</td>
 			</tr>
+		<?php endif; ?>
 
 		<?php endforeach; ?>
 	</body>
