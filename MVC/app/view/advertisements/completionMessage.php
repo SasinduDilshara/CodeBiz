e39a9ceb6032d391b3b9702b66759b16df57d916
Shammi <shammi0107@gmail.com>
2019-04-14 23:27:56 +0530

    Create completionMessage.php

---
+++ MVC/app/view/advertisements/completionMessage.php
@@ -0,0 +1,11 @@
+<?php $this->setSiteTitle( 'Request Completed'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<p> Your Completion was inform to the <?= $this->servicer->username ?> </p>
+
+<a href="<?=PROOT?>register/confirmed/<?= $this->servicer->id ?>/<?= $this->request->id ?>" class="btn btn-xs btn-default"> Rate servicer </a>
+<a href="<?=PROOT?>requests/ShowConfirmRequests" class="btn btn-xs btn-default"> Back</a>
+
+<?php $this->end(); ?>
