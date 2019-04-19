54ede687485b8c4b51aed5744985dce4fb56c479
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-20 01:35:27 +0530

    Create getNewPassForm.php

---
+++ MVC/app/view/emails/getNewPassForm.php
@@ -0,0 +1,26 @@
+<?php $this->setSiteTitle( 'Forget Password'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<form class="form" action='<?=PROOT?>emails/getNewPassword/<?=$this->id?>' method="post">
+
+<p>Type Your Email here. We will send you a form to fill out</p>
+
+
+	<div class="form-group col">
+		<?= input_block('text','pass','pass','',['class'=>'form-control'],['class'=>'form-group']) ?>
+		<ul class="list-group" id="result"></ul>
+
+		<div class="text-center">
+            <input type="submit" class="btn btn-xs btn-primary" value="Set Password" >
+        </div>
+
+	</div>
+
+
+
+<a href="<?=PROOT?>" class="btn btn-xs btn-default" > Back to Boardingvibes </a>
+</form>
+
+<?php $this->end(); ?>
