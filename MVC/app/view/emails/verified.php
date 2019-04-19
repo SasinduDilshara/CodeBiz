652769f4b2fbc993f3c5219ec635d46b4a3247a2
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-20 01:35:23 +0530

    Create verified.php

---
+++ MVC/app/view/emails/verified.php
@@ -0,0 +1,13 @@
+<?php $this->setSiteTitle( 'Verified Email'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<p>Your Email verification has been completed</p>
+
+<a href="<?=PROOT?>" class="btn btn-xs btn-default" > Back to Boardingvibes </a>
+
+<?php $this->end(); ?>
+
+
+
