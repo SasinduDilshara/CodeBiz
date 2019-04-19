f768f7428cfce16a86878fc5189c22b6aa8150cf
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-20 01:35:37 +0530

    Create forgotmsgsend.php

---
+++ MVC/app/view/emails/forgotmsgsend.php
@@ -0,0 +1,13 @@
+<?php $this->setSiteTitle( 'Verified Email'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<p> Check Your Email to reset your password </p>
+
+<a href="<?=PROOT?>" class="btn btn-xs btn-default" > Back to Boardingvibes </a>
+
+<?php $this->end(); ?>
+
+
+
