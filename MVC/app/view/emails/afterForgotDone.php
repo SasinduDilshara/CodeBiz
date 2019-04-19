2aff8fc38e43696f2cf6c4aa91ddadd87011bce0
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-20 01:36:07 +0530

    Create afterForgotDone.php

---
+++ MVC/app/view/emails/afterForgotDone.php
@@ -0,0 +1,13 @@
+<?php $this->setSiteTitle( 'Verified Email'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<p> Your Password Has Changed </p>
+
+<a href="<?=PROOT?>/register/login" class="btn btn-xs btn-default" > Back to Boardingvibes </a>
+
+<?php $this->end(); ?>
+
+
+
