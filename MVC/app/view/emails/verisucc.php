873b689658156126844ca0f1348d31cf9add645e
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-04-20 01:35:20 +0530

    Create verisucc.php

---
+++ MVC/app/view/emails/verisucc.php
@@ -0,0 +1,13 @@
+<?php $this->setSiteTitle( 'Send Email'); ?>
+<?php $this->start('head'); ?>
+<?php $this->end(); ?>
+<?php $this->start('body'); ?>
+
+<p>Check out your email to verify your email address</p>
+
+<a href="<?=PROOT?>/register/login" class="btn btn-xs btn-default" > Back to Boardingvibes </a>
+
+<?php $this->end(); ?>
+
+
+
