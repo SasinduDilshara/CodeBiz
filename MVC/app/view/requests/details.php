a2ea2ae6626bc8944fa5c072bf1c60d4451cb4d1
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-04-22 14:39:17 +0530

    Update details.php

--- MVC/app/view/requests/details.php
+++ MVC/app/view/requests/details.php
@@ -3,21 +3,28 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<div class="col-md-8 col-md-offset-2 well">
-	<?php if(currentUser()->userType == 'Customer'): ?>
-	<a href="<?=PROOT?>requests" class="btn btn-xs btn-default"> Back</a>
-	<?php endif; ?>
-	<?php if(currentUser()->userType == 'Provider'): ?>
-	<a href="<?=PROOT?>requests/search?area=<?=$this->request->area?>" class="btn btn-xs btn-default"> Back</a>
-	<?php endif; ?>
-
-
-
-
-	<h2 class="text-center"><?=$this->request->service?></h2>
-	<div class="col-md-6">
-		<p><strong><pre> Description :  </strong><?=$this->request->description?></pre></p>
-		
+<div class="card border-primary mb-3" style="max-width: 40rem; margin:auto; top:5rem;">
+	<div class="card-header" style="text-align: center;padding-top: 5px;padding-bottom: 5px;">
+		Request Details
+	</div>
+	<div class="card-body" style="background-color:#fdfdfe">
+		<h4><?= $this->request->service; ?></h4>
+		<table class="table table-hover">
+			<thead>
+				<tr class="table-light">
+				<td><strong>  Description :</strong></td>
+				<td><?=$this->request->description?></td>
+				</tr>
+			</thead>
+		</table>
+		<div class="form-group text-right">
+			<?php if(currentUser()->userType == 'Customer'): ?>
+				<a href="<?=PROOT?>requests" class="btn btn-xs btn-default"> Back</a>
+			<?php endif; ?>
+			<?php if(currentUser()->userType == 'Provider'): ?>
+				<a href="<?=PROOT?>requests/search?area=<?=$this->request->area?>" class="btn btn-xs btn-default"> Back</a>
+			<?php endif; ?>
+		</div>
 	</div>
 </div>
 
