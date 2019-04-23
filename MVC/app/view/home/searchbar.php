61571e7f3526756ded9ef22ce118b811fd84425b
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-04-23 12:26:50 +0530

    Update searchbar.php

--- MVC/app/view/home/searchbar.php
+++ MVC/app/view/home/searchbar.php
@@ -1,10 +1,10 @@
 <div style="text-align:center">
-    <div class="text-white text-uppercase" style="font-family:Sans-serif;font-size:4rem;padding:6rem 1rem;">Find Your Services</div>
-    <div class="navbar navbar-expand-sm navbar-light" style="display:inline-block;background-color:rgba(0,0,0,0.8);">
+    <div class="text-white text-uppercase" style="font-family:Sans-serif;font-size:4rem;padding:5rem 1rem;">Find Your Services</div>
+    <div class="navbar navbar-expand-sm navbar-light" style="display:inline-block;background-color:rgba(0,0,0,0.8);padding:8px;">
     <!-- <h3 class="text-center"> Register  </h3> -->
         <form class="form-inline my-2 my-lg-0"action="advertisements/search" method="GET">
         <ul class="navbar-nav mr-auto">
-                <li class="nav-item col bg-light" style="padding:12px;">
+                <li class="nav-item col bg-light" style="padding:6px;">
                     <div class="input-select">
                         <select data-trigger="" class="custom-select"name="type" required>
                             <option disabled selected value>I'm looking for</option>
@@ -14,10 +14,10 @@
                         </select>
                     </div>
                 </li>
-                <li class="nav-item bg-light" style="padding:12px;">
+                <li class="nav-item bg-light" style="padding:6px;">
                     <input type="text" name="area" id="area" class="form-control" value="" placeholder="I live in">
                 </li>
-                <li class="nav-item col bg-light" style="padding:12px;">
+                <li class="nav-item col bg-light" style="padding:6px;">
                     <input type="submit" class="btn btn-secondary my-2 my-sm-0" value="Search" >
                 </li>
             </ul>
