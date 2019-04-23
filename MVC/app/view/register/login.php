4745637304a1fe2cf522e6b85431985edaaed979
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-04-23 13:11:51 +0530

    Update login.php

--- MVC/app/view/register/login.php
+++ MVC/app/view/register/login.php
@@ -4,20 +4,35 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<form class="form-signin">
-  <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
-</form>
+<div class = "card border-primary mb-3" style="max-width: 30rem; margin:auto; top:8rem; background-color: ;" >
+ 	<div class="card-header" style="text-align: center">
+		 Login
+	</div>
+	<!-- <h3 class="text-center p-4">/h3> -->
+	<div class="card-body">
+	<form class="form" action="<?=PROOT?>register/login" method="post" ><br>
+		<div class="form-group">
+			<input type="text" name="username" id="username" class = "form-control" placeholder = "Username">
+		</div>
+		<div class="form-group">
+			<input type="password" name="password" id="password" class="form-control" placeholder = "Password">
+		</div>
+		<div class="form-check">
+			<input type="checkbox" class="form-check-input" name="remember_me" id="remember_me" value="on">
+			<label class="form-check-label" for="remember_me" > Remember me
+			</label>
+		</div>
+		<div> <?= $this->displayErrors; ?> </div>
+		<div class="form-group" style="text-align: center;">
+			<input type="submit" class ="btn btn-primary" value="Login" >
+		</div>
+	</form>
+
+	<div>
+		<a href = "<?=PROOT?>emails/forgetpassword"  class ="text-primary"> Forget password ? </a>
+	</div>
+
+</div>
+</div>
 
 <?php $this->end(); ?>
