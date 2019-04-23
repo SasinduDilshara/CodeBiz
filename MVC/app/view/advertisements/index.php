5eb4d4c6919d1b7e1d41b12a9a67ac64f61c6e8f
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-04-23 17:34:24 +0530

    Revert "Tables for ads and requests"
    This reverts commit ab62ad18e3e6d2472cf407b58c64a77839f008be.

--- MVC/app/view/advertisements/index.php
+++ MVC/app/view/advertisements/index.php
@@ -2,38 +2,23 @@
 <?php $this->setSiteTitle("Advertisements"); ?>
 <?php $this->start('body'); ?>
 <!-- <h1 class ="text-center red"> My Advertisements </h1> -->
-<div class="text-center text-white text-uppercase" style="font-family:Sans-serif;font-size:2rem;padding-top:3rem;">My Advertisements</div>
-<div class = "card border-primary mb-3" style="max-width: 80rem; margin:auto;">
-	<table class="table table-hover">
-		<thead>
-			<tr class="table-light">
-			<th scope="col">Topic</th>
-			<th scope="col">Description</th>
-			<th scope="col">Location</th>
-			<th scope="col">Type</th>
-			<th scope="col"></th>
-			</tr>
-		</thead>
-		<tbody>
-		<?php foreach($this->alladds as $advertisements): ?>
-			<?php foreach($advertisements as $advertisement): ?>
-				<tr>
-					<th scope="row">
-						<a href="<?=PROOT?>advertisements/details/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link"><?= $advertisement->topic ?></a>
-					</th>
-					<td><?= $advertisement->description; ?></td>
-					<td><?= $advertisement->area; ?></td>
-					<td><?= $advertisement->type;?></td>
-					<td>
-						<a href="<?=PROOT?>advertisements/edit/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link"> Edit </a>
-						<a href="<?=PROOT?>advertisements/showAccept/<?=$advertisement->id ?>/<?=$advertisement->type?>" class="card-link"> Show Acceptences </a>
-						<a href="<?=PROOT?>advertisements/delete/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link" onclick="if(!confirm('Are you sure you want to delete this?')){return false;}"></i> Delete </a>
-					</td>
-				</tr>
-			<?php endforeach; ?>
+<div class="col-10" style="margin:auto;top:3rem;">
+	<?php foreach($this->alladds as $advertisements): ?>
+		<?php foreach($advertisements as $advertisement): ?>
+			<div class="card border-primary mb-3" style="">
+				<div class="card-body">
+					<h4><?= $advertisement->topic ?></h4>
+					<p class="card-text"><strong>Description : </strong><?= $advertisement->description ?></p>
+					<p class="card-text"><strong>Area : </strong><?= $advertisement->area ?></p>
+					<p class="card-text"><strong>Type : </strong><?= $advertisement->type ?></p>
+					<a href="<?=PROOT?>advertisements/details/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link"> Details </a>
+					<a href="<?=PROOT?>advertisements/edit/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link"> Edit </a>
+					<a href="<?=PROOT?>advertisements/delete/<?=$advertisement->type?>/<?=$advertisement->id?>" class="card-link" onclick="if(!confirm('Are you sure you want to delete this?')){return false;}"></i> Delete </a>
+					<a href="<?=PROOT?>advertisements/showAccept/<?=$advertisement->id ?>/<?=$advertisement->type?>" class="card-link"> Show Acceptences </a>
+				</div>
+			</div>
 		<?php endforeach; ?>
-		</tbody>
-	</table>
+	<?php endforeach; ?>
 </div>
 <?php $this->end(); ?>
 
--- MVC/app/view/requests/index.php
+++ MVC/app/view/requests/index.php
@@ -1,35 +1,20 @@
 
 <?php $this->setSiteTitle("Requests"); ?>
 <?php $this->start('body'); ?>
-<!-- <h1 class ="text-center red"> My Requests </h1> -->
-<div class="text-center text-white text-uppercase" style="font-family:Sans-serif;font-size:2rem;padding-top:3rem;">My Requsets</div>
-<div class = "card border-primary mb-3" style="max-width: 80rem; margin:auto;">
-	<table class="table table-hover">
-		<thead>
-			<tr class="table-light">
-			<th scope="col">Topic</th>
-			<th scope="col">Description</th>
-			<th scope="col">Location</th>
-			<th scope="col"></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach($this->requests as $request): ?>
-				<tr>
-					<th scope="row">
-						<a href="<?=PROOT?>requests/details/<?=$request->id?>" class="card-link"><?= $request->service; ?></a>
-					</th>
-					<td><?= $request->description; ?></td>
-					<td><?= $request->area; ?></td>
-					<td>
-					<a href="<?=PROOT?>requests/edit/<?=$request->id?>" class="card-link">Edit</a>
-					<a href="<?=PROOT?>requests/showAccept/<?=$request->id?>" class="card-link">Show Acceptences </a>
-					<a href="<?=PROOT?>requests/delete/<?=$request->id?>" class="card-link" style="" onclick="if(!confirm('Are you sure you want to delete <?=$request->service;?>?')){return false;}">Delete</a>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
+<!-- <h1 class ="text-center red"> My Requests </h1> --><br>
+<div class="col-10" style="margin:auto;top:3rem;">
+	<?php foreach($this->requests as $request): ?>
+		<div class="card border-primary mb-3" style="">
+			<div class="card-body">
+				<h4><?= $request->service; ?></h4>
+				<p class="card-text"><?= $request->description; ?></p>
+				<a href="<?=PROOT?>requests/details/<?=$request->id?>" class="card-link">Details</a>
+				<a href="<?=PROOT?>requests/edit/<?=$request->id?>" class="card-link">Edit</a>
+				<a href="<?=PROOT?>requests/delete/<?=$request->id?>" class="card-link" style="" onclick="if(!confirm('Are you sure you want to delete <?=$request->service;?>?')){return false;}">Delete</a>
+				<a href="<?=PROOT?>requests/showAccept/<?=$request->id?>" class="card-link">Show Acceptences </a>
+			</div>
+		</div>
+	<?php endforeach; ?>
 </div>
 
 <?php $this->end(); ?>
