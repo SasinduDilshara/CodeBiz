3854fb824fabf2cf034e2bb61e6cbed06ca185f3
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-17 10:46:37 +0530

    Update showChat.php

--- MVC/app/view/advertisements/showChat.php
+++ MVC/app/view/advertisements/showChat.php
@@ -6,6 +6,7 @@
 <div class = "card border-primary mb-3" style="max-width: 50rem; margin:auto; top:5rem;">
 	<div class="card-header" style="text-align: center"> Chat </div>
 	<div class="card-body">
+	<?php if($this->chat):?>
         <?php foreach($this->chat as $chatbar): 
 			$chat = explode(" MESSAGE??: ", $chatbar);
 			$message = $chat[1];
@@ -20,7 +21,10 @@
             </div>
 			<?php endif; ?>  
         <?php endforeach; ?>
-
+	<?php else: ?>
+		<div style="text-align: center"> Chat is empty</div>
+	<?php endif; ?>
+		<br>
         <div class="form-group text-center">
             <a href="<?=PROOT?>advertisements/askQuestion/<?=$this->advertisement->id?>/<?=$this->advertisement->user_id?>/<?= $this->advertisement->type ?>" class="btn btn-primary"></i> Send Message </a>
             <a onclick="window.history.back();" class="btn btn-secondary" > Back </a>
