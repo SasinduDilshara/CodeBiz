e1a5412e9ea55c5560f9946b2ebfcf44a3fccc7d
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-17 09:15:37 +0530

    Update Users.php

--- MVC/app/view/advertisements/showAccept.php
+++ MVC/app/view/advertisements/showAccept.php
@@ -1,63 +1,40 @@
 <?php $this->setSiteTitle("Accepted Customers"); ?>
 <?php $this->start('body'); ?>
-<h1 class ="text-center red"> Accepted Customers </h1>
-<table class="table table-striped table-condensed table-bordered">
+<div class="text-center text-white text-uppercase" style="font-family:Sans-serif;font-size:2rem;padding-top:5rem;">Accepted Customers - <?=$this->advertisement->topic?></div>
+
+<div class = "card border-primary mb-3" style="max-width: 80rem; margin:auto;">
+	<table class="table table-hover">
 	<thead>
 		<th> Customer</th>
-		<th> Date</th>
-		<th> Time</th>
 		<th></th>
 	</thead>
-	<body>
+	<tbody>
 		<?php foreach($this->customerList as $customer): ?>
-
-	<tr>
-
-		<td>
-		   <a   
-			href="<?=PROOT?>accounts/details/<?=$customer->id?>">
-					<?= $customer->username; ?>
-			</a>
-			</td>
-				<td><?= $this->date ?></td>
-				<td><?= $this->time ?></td>
-				<td>
-				<?php if(!in_array((string)($customer->id),explode(",",($this->advertisement->confirmCustomerId)))):?>
-				<a href="<?=PROOT?>advertisements/confirm/<?=$this->advertisement->id?>/<?=$customer->id?>/<?=$this->advertisement->type?>" class="btn btn-danger btn-xs"></i> Accept </a>
-				<?php endif; ?>
-
-				<?php if(in_array((string)($customer->id),explode(",",($this->advertisement->confirmCustomerId)))) :?>
-				<a href="<?=PROOT?>advertisements/cancelConfirm/<?=$this->advertisement->id?>/<?=$customer->id?>/<?=$this->advertisement->type?>" class="btn btn-danger btn-xs"></i> Cancel Acceptance </a>
-				<?php endif; ?>
-			</td>
-				<?php if(in_array((string)($customer->id),explode(",",($this->advertisement->confirmCustomerId)))):?>
+			<tr>
 
 				<td>
-
+				<a href="<?=PROOT?>accounts/details/<?=$customer->id?>"><?= $customer->username; ?></a>
 				</td>
-				<?php endif; ?>
+				<td>
+					<?php if(!in_array((string)($customer->id),explode(",",($this->advertisement->confirmCustomerId)))):?>
+						<a href="<?=PROOT?>advertisements/confirm/<?=$this->advertisement->id?>/<?=$customer->id?>/<?=$this->advertisement->type?>" > Accept </a>
+					<?php endif; ?>
+					<?php if(in_array((string)($customer->id),explode(",",($this->advertisement->confirmCustomerId)))) :?>
+						<a href="<?=PROOT?>advertisements/cancelConfirm/<?=$this->advertisement->id?>/<?=$customer->id?>/<?=$this->advertisement->type?>" > Cancel Acceptance </a>
+					<?php endif; ?>
+				</td>
 				<td>
 					<?php if(!in_array($customer->username,explode(",",$this->advertisement->ratedType)) && in_array((string)($customer->id),explode(",",$this->advertisement->confirmCustomerId))): ?>
-					<a href="<?=PROOT?>register/confirmedADD/<?=$customer->id?>/<?=$this->advertisement->id?>/<?=$this->advertisement->type?>" class="btn btn-danger btn-xs"> Rate <?= $customer->username ?> and Finish</a>
-				
-
-					<?php elseif(true): ?>
-					<a  class="btn btn-danger btn-xs" disable > Rate <?= $customer->username ?> and Finish</a>
-				<?php endif; ?>
-
-				</td>
+						<a href="<?=PROOT?>register/confirmedADD/<?=$customer->id?>/<?=$this->advertisement->id?>/<?=$this->advertisement->type?>" class=""> Rate <?= $customer->username ?> and Finish</a>
+					<?php endif; ?>
 				</td>
 			</tr>
-
 		<?php endforeach; ?>
-
-											<a href="<?=PROOT?>advertisements/askQuestion/<?=$this->advertisement->id?>/<?=$this->advertisement->user_id?>/<?=$this->advertisement->type?>/<?= $customer->username ?>" class="btn btn-danger btn-xs"></i> Message to customers  </a>
-					<a href="<?=PROOT?>advertisements/showChat/<?=$this->advertisement->id?>/<?=$this->advertisement->type?>" class="btn btn-danger btn-xs"></i> Show Chat </a>
-
-	</body>
-</table>
+	</tbody>
+	</table>
+</div>
+					
 <?php $this->end(); ?>
-
 
 
 
