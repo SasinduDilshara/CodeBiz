df31d751aaaeca64fad393be7ddaaf8fcb26c96b
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-25 18:11:13 +0530

    Update succefulAskedQuestion.php

--- MVC/app/view/requests/succefulAskedQuestion.php
+++ MVC/app/view/requests/succefulAskedQuestion.php
@@ -4,7 +4,7 @@
 <?php $this->start('body'); ?>
 
 <div class = "alert alert-primary" style="max-width: 30rem; margin:auto; top:5rem;">
-    <button type="button" class="close" onclick="window.location.href='<?=PROOT?>requests/ShowConfirmRequests';" ?>&times;</button>
+    <button type="button" class="close" onclick="window.history.back;" ?>&times;</button>
 	<div class="card-body">
         <p class="mb-0"> Your question has been informed to <?= $this->customer->username ?> </p>
 	</div>
