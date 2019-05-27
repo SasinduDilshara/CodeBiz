e84615a47854f3fad89d92a1650151ca051bd421
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-28 01:42:22 +0530

    photo

--- MVC/app/view/accounts/index.php
+++ MVC/app/view/accounts/index.php
@@ -2,28 +2,39 @@
 <?php $this->start('head'); ?>
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
-
+<style>
+.photo
+{
+	background: url(<?=PROOT?>img/upload/<?=$this->account->photolink?>) 50% 50% no-repeat, url(<?=PROOT?>img/user.png);
+	background-size: cover;
+	width: 200px;
+  	height: 200px;
+	margin: auto;
+	border-radius: 50%;
+	border-style: outset;
+}
+</style>
 <div class="card border-primary mb-3" style="max-width: 30rem; margin:auto; top:5rem;">
 	<div class="card-header" style="text-align: center;padding-top: 5px;padding-bottom: 5px;">
 		User Details
 	</div>
-	
 	<div class="card-body" style="background-color:#fdfdfe">
+		<div class="photo"></div><br>
 		<table class="table table-hover">
 			<thead>
 			<tr>
 				<form action="<?=PROOT?>accounts/upload/<?=currentUser()->id?>" method="POST" enctype="multipart/form-data">
-				<input type="file" name="file" id="">
-				<button type="submit" name="submit">UPLOAD</button>
+					<div class="input-group mb-3">
+						<div class="custom-file">
+							<input type="file" class="custom-file-input" id="file" name="file">
+							<label class="custom-file-label" for="file">Change Profile Photo</label>
+						</div>
+						<div class="input-group-append">
+							<button type="submit" name="submit" class="input-group-text">Upload</button>
+						</div>
+					</div>
 				</form>
 			</tr>
-
-
-<tr>
-				<td><strong>  photo :</strong></td>
-				<td><?=$this->account->photolink?></td>
-</tr>
-
 				<tr>
 				<td><strong>  Name :</strong></td>
 				<td><?=$this->account->displayName()?></td>
