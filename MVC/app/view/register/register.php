0c5234bb629d270d4ef956f28f751e90244c4748
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-28 02:05:40 +0530

    Update register.php

--- MVC/app/view/register/register.php
+++ MVC/app/view/register/register.php
@@ -75,23 +75,17 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6">
-                <label for="area"> City* </label>
-                <input type="text" name="area" id="area" class="form-control" required title="must be a valid address" value="<?=$this->post['area'] ?>">
-                <!-- <ul class="list-group" id="result"></ul> -->
+            <div class="form-row col">
+                <div class="col">
+                    <label for="area"> City* </label>
+                    <input type="text" name="area" id="area" class="form-control" required title="must be a valid address" value="<?=$this->post['area'] ?>">
+                </div>
+                <div class="col">
+                    <div class="form-group"><label for="type">Type</label><input type="text" value="<?= $this->type ?>" class="form-control" disabled></div>
+                </div>
             </div>
-
-            <div class="col-6">
-                <label for="profilePhoto">Profile Photo</label>
-                <input 
-                type="file" 
-                name="profilePhoto"
-                class="form-control-file" 
-                id="profilePhoto" 
-                aria-describedby="fileHelp"
-                value="<?=$this->post['profilePhoto'] ?>">
-            <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
-    </div>
+           
+            
             <div><?=$this->displayErrors ?></div>
             <div class="text-center">
                 <input type="submit" class="btn btn-xs btn-primary" value="Register" >
