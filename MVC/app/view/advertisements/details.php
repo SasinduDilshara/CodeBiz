779793956d603e150e18740b1a8223bbb76929ca
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-05-30 00:13:59 +0530

    a

--- MVC/app/view/advertisements/details.php
+++ MVC/app/view/advertisements/details.php
@@ -102,8 +102,10 @@
 				<a class="btn btn-outline-danger" style="margin: 13px 12px 12px 10px;" href="<?=PROOT?>advertisements/report/advertisements/<?=$this->advertisement->id?>/<?=currentUser()->id?>/<?= $this->advertisement->type?>" onclick="if(!confirm('Are you sure you want to report this advertisement?')){return false;}"> Report This Add </a>
 			<?php endif; ?>
 		<?php endif; ?>
+		<?php if(currentUser()): ?>
 		<?php if(in_array((string)(currentUser()->id),explode(",",$this->advertisement->reportedBy))):?>
 			<button class="btn btn-danger" style="margin: 13px 12px 12px 10px;" disabled>Reported</button> 
+		<?php endif; ?>
 		<?php endif; ?>
 		<div class = "form-group text-right">
 			<a onclick="window.history.back();" class="btn btn-default"> Back </a>
