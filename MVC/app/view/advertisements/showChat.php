ec6e1d4bd95462c06cac1779959868eade3f040c
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-30 08:53:12 +0530

    chat

--- MVC/app/view/advertisements/showChat.php
+++ MVC/app/view/advertisements/showChat.php
@@ -3,9 +3,10 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<div class = "card border-primary mb-3" style="max-width: 40rem; margin:auto; top:5rem;">
+<div class = "card border-primary mb-3" style="max-width: 30rem; margin:auto; top:5rem;">
 	<div class="card-header" style="text-align: center"> Chat </div>
-	<div class="card-body">
+	<div class="card-body" style="background-color:#001128">
+    <div style="background:url(<?=PROOT?>img/chat.jpg) 50% 50% no-repeat;height: 30rem;overflow: auto;">
 	<?php if($this->chat):?>
         <?php foreach($this->chat as $chatbar): 
 			$chat = explode(" MESSAGE??: ", $chatbar);
@@ -17,27 +18,30 @@
 			$from = substr($chat[0], 8);
 			$from = base64_decode($from);
 			if($to == currentUser()->username):?>
-            <div class="btn btn-secondary" style=" margin: 3px;">
-            <strong><?=$from?>:</strong> <?=$message?>
-            </div> <br>
+			<div class="text-left">
+				<div class="btn btn-secondary" style=" margin: 3px;">
+				<strong><?=$from?> : </strong> <?=$message?>
+				</div>
+			</div>
 			<?php endif;
 			if($from == currentUser()->username ):?>
 			<div class="text-right">
 				<div class="btn btn-info" style=" margin: 3px;">
-					<?php if(currentUser()->userType=="Customer"): ?>
-				<strong><?=$from?>:</strong> <?=$message?>
+				<?php if(currentUser()->userType=="Customer"): ?>
+					</strong> <?=$message?>
 				<?php else: ?>
-						<strong><?=$from?>:</strong> <?=$message?> <strong>@<?=$to?></strong>
+					<strong>@<?=$to?> : </strong><?=$message?> 
 				<?php endif; ?>
-				</div> <br>
+				</div>
 			</div>  
 			<?php endif; ?> 
         <?php endforeach; ?>
 	<?php else: ?>
 		<div style="text-align: center"> Chat is empty</div>
 	<?php endif; ?>
+	</div>
 		<br>
-        <div class="form-group text-center">
+        <div class="form-group text-center" style="margin-bottom: 0px;">
             <a href="<?=PROOT?>advertisements/askQuestion/<?=$this->advertisement->id?>/<?=$this->advertisement->user_id?>/<?= $this->advertisement->type ?>" class="btn btn-primary"></i> Send Message </a>
 <?php if(currentUser()->userType == "Provider"): ?>
          <a href="<?=PROOT?>advertisements/showAccept/<?= $this->id ?>/<?= $this->type ?>" class="btn btn-secondary" > Back </a>
--- MVC/app/view/requests/showChat.php
+++ MVC/app/view/requests/showChat.php
@@ -3,35 +3,37 @@
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
 
-<div class = "card border-primary mb-3" style="max-width: 40rem; margin:auto; top:5rem;">
-	<div class="card-header" style="text-align: center"> Chat </div>
-	<div class="card-body">
-        <?php if($this->chat):?>
+<div class = "card border-primary mb-3" style="max-width: 30rem; margin:auto; top:5rem;">
+	<!-- <div class="card-header" style="text-align: center"> Chat </div> -->
+	<div class="card-body" style="background-color:#001128">
+    <div style="background:url(<?=PROOT?>img/chat.jpg) 50% 50% no-repeat;height: 30rem;overflow: auto;">
+    <?php if($this->chat):?>
         <?php foreach($this->chat as $chatbar):
 
             $chatarr = explode(':',$chatbar);
             // dnd($chatarr); 
             ?>
-        
-            <div class="alert alert-info">
-                <?php if($chatarr[0] == (currentUser()->username)." "): ?>
-                    <div class="text-right">
-                <div class="btn btn-info" style=" margin: 3px;">
-                    <strong><?= $chatarr[0] ?>:</strong> <?= base64_decode($chatarr[1]) ?>
+            <?php if($chatarr[0] == (currentUser()->username)." "): ?>
+            <div class="text-right">
+				<div class="btn btn-info" style=" margin: 3px;">
+                    <?= base64_decode($chatarr[1]) ?>
                 </div>
             </div>
             <?php elseif($chatarr[0] != (currentUser()->username)." "): ?>
-            <div class="btn btn-secondary" style=" margin: 3px;">
-            <strong><?= $chatarr[0] ?>:</strong> <?= base64_decode($chatarr[1]) ?>
-            </div> <br>
+            <div class="text-left">
+                <div class="btn btn-primary" style=" margin: 3px;">
+                    <strong><?= $chatarr[0] ?> : </strong> <?= base64_decode($chatarr[1]) ?>
+                </div>
+            </div>
             <?php endif; ?>
-            </div>   
+            <!-- </div>    -->
         <?php endforeach; ?>
-        <?php else: ?>
+    <?php else: ?>
         <div style="text-align: center"> Chat is empty</div>
-        <?php endif; ?>
+    <?php endif; ?>
+    </div>
         <br>
-        <div class="form-group text-center">
+        <div class="form-group text-center" style="margin-bottom: 0px;">
             <a href="<?=PROOT?>Requests/clearChat/<?=$this->request->id ?>" class="btn btn-danger" > Delete Chat </a>
             <a href="<?=PROOT?>requests/askQuestion/<?=$this->request->id?>/<?=$this->request->user_id?>" class="btn btn-primary"></i> Send Message </a>
             <?php if(currentUser()->userType == "Provider"): ?>
