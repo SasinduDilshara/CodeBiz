9854288056c6d9f561c72b67923bb69fc465b687
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-05-30 06:12:09 +0530

    Update showChat.php

--- MVC/app/view/requests/showChat.php
+++ MVC/app/view/requests/showChat.php
@@ -14,7 +14,17 @@
             ?>
         
             <div class="alert alert-info">
+                <?php if($chatarr[0] == (currentUser()->username)." "): ?>
+                    <div class="text-right">
+                <div class="btn btn-info" style=" margin: 3px;">
+                    <strong><?= $chatarr[0] ?>:</strong> <?= base64_decode($chatarr[1]) ?>
+                </div>
+            </div>
+            <?php elseif($chatarr[0] != (currentUser()->username)." "): ?>
+            <div class="btn btn-secondary" style=" margin: 3px;">
             <strong><?= $chatarr[0] ?>:</strong> <?= base64_decode($chatarr[1]) ?>
+            </div> <br>
+            <?php endif; ?>
             </div>   
         <?php endforeach; ?>
         <?php else: ?>
@@ -24,8 +34,13 @@
         <div class="form-group text-center">
             <a href="<?=PROOT?>Requests/clearChat/<?=$this->request->id ?>" class="btn btn-danger" > Delete Chat </a>
             <a href="<?=PROOT?>requests/askQuestion/<?=$this->request->id?>/<?=$this->request->user_id?>" class="btn btn-primary"></i> Send Message </a>
-            <a onclick="window.history.back();" class="btn btn-secondary" > Back </a>
+            <?php if(currentUser()->userType == "Provider"): ?>
+         <a href="<?=PROOT?>requests/ShowConfirmRequests" class="btn btn-secondary" > Back </a>
+       <?php endif; ?>
+<?php if(currentUser()->userType == "Customer"): ?>
+         <a href="<?=PROOT?>requests/ShowConfirmRequests" class="btn btn-secondary"> Back </a>
         </div>
+        <?php endif; ?>
 	</div>
 </div>
 
