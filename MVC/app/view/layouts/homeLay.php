bcda6301dd2f56b0b9a28b15db2ca3a7de0473f3
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-30 10:06:17 +0530

    Update homeLay.php

--- MVC/app/view/layouts/homeLay.php
+++ MVC/app/view/layouts/homeLay.php
@@ -11,7 +11,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 
-
+        <title><?= $this->siteTitle() ?></title>
 
 
         <link rel="stylesheet" type="text/css" href="<?=PROOT?>css/bootstrap.min.css" />
