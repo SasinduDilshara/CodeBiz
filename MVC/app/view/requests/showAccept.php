c6690e3ecc075dbdd98fc019079f2459a7fd3571
hwupathum <46132469+hwupathum@users.noreply.github.com>
2019-05-30 09:48:15 +0530

    Update showAccept.php

--- MVC/app/view/requests/showAccept.php
+++ MVC/app/view/requests/showAccept.php
@@ -1,15 +1,10 @@
 <?php $this->setSiteTitle("Accepted Providers"); ?>
 <?php $this->start('body'); ?>
-<div class="text-center text-white text-uppercase" style="font-family:Sans-serif;font-size:2rem;padding-top:5rem;"><?=$this->request->service ?></div>
-<div class = "card border-primary mb-3" style="max-width: 80rem; margin:auto;">
+<div id="title" class="text-center text-white text-uppercase" style="font-family:Sans-serif;font-size:2rem;padding-top:5rem;text-shadow: 3px 4px 5px #000;"><?=$this->request->service ?></div>
+<div class = "card border-primary mb-3" style="max-width: 30rem; margin:auto;">
 
 	<table class="table table-hover">
-		<thead>
-			<th> Provider</th>
-			<th><button type="button" class="close" onclick="location.href='<?=PROOT?>requests'">&times;</button></th>
-
-		</thead>
-	<tbody>
+	<thead>
 		<?php foreach($this->providerslist as $provider): ?>
 			<tr>
 				<td>
@@ -17,22 +12,22 @@
 				</td>
 				<td>
 				<?php if($this->request->confirmProviderId == 0):?>
-					<a href="<?=PROOT?>requests/confirm/<?=$this->request->id?>/<?=$provider->id?>" > Confirm </a>
+					<a class="btn btn-outline-primary" href="<?=PROOT?>requests/confirm/<?=$this->request->id?>/<?=$provider->id?>" > Confirm </a>
 				<?php endif; ?>
 				<?php if((($this->request->confirmProviderId != $provider->id) && ($this->request->confirmProviderId != 0)) ):?>
-					<a disable > Not selected </a>
+					<button class="btn btn-primary" disabled > Not selected </button>
 				<?php endif; ?>
 				<?php if($this->request->completed == 1 && $this->request->confirmProviderId == $provider->id):?>
-					<a  disabled > Finished </a>
+					<button  class="btn btn-primary"disabled > Finished </button>
 				<?php endif; ?>
 				<?php if($this->request->confirmProviderId == $provider->id):?>
 					<?php if($this->request->completed == 0): ?>
-						<a href="<?=PROOT?>requests/cancelConfirm/<?=$this->request->id?>/<?=$provider->id?>" > Cancel Confirmation </a>
+						<a class="btn btn-outline-primary" href="<?=PROOT?>requests/cancelConfirm/<?=$this->request->id?>/<?=$provider->id?>" > Cancel Confirmation </a>
 					<?php endif;?><?php endif;?>
 				</td>
 			</tr>
 		<?php endforeach; ?>
-	</tbody>
+	</thead>
 </table>
 <?php $this->end(); ?>
 
